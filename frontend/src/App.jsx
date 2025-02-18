cd77415a8038611b79353f836896e2cad5e81eaf
Jonathan Scruggs <30415863+Jonathan-Scruggs@users.noreply.github.com>
2025-02-18 13:05:09 -0700

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-     <h1> Hello World </h1>
-    </div>
+   <Router>
+      <Routes>
+        <Route path="/" element={}/>
+
+      </Routes>
+
+
+
+   </Router>
   );
 }
 
