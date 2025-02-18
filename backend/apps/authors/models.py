1497699ab7d674767dd25959629ed64dc227ad68
Krish <krishlal4204@gmail.com>
2025-02-18 01:10:57 -0700

    lay foundation for the login feature

--- backend/apps/authors/models.py
+++ backend/apps/authors/models.py
@@ -1,11 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Author(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='author_profile'
+    )
+
     displayName = models.CharField(max_length=256)
     host = models.URLField()
     github = models.URLField()
     #not sure if this is gonna be just the url the frontend pulls or straight up the image, so it could be models.ImageField()
     profileImageURL = models.URLField()
     page = models.URLField()
+
+    # basically we'll use this in the future to identify whether or not our current Author is from another node
+    is_local = models.BooleanField(default=True)
+    username = models.CharField(max_length=128, unique=True)
+
+    #we'll use this to identify which authors haven't been onboarded into the system or soft deleted
+    allowed_states = (
+        ('PENDING', 'PENDING'),
+        ('ACTIVE', 'ACTIVE'),
+        ('DELETED', 'DELETED')
+    )
+    state = models.CharField(max_length=7, choices=allowed_states, default='PENDING')
--- backend/apps/authors/views.py
+++ backend/apps/authors/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework import status
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.decorators import method_decorator
+from .models import Author
+
 
 # Create your views here.
+class AuthorLoginView(APIView):
+    """
+    This will be the path handling all login work
+    We'll take the info from the frontend and authenticate a user n use django sessions
+    to sustain it
+    """
+
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({'detail': 'CSRF cookie set.'})
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return JsonResponse({'error': 'Missing Username or Password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        auth_user = authenticate(request, username=username, password=password)
+        if auth_user is not None:
+            try:
+                author = auth_user.author_profile
+            except Author.DoesNotExist:
+                return JsonResponse({'error': 'No author found'}, status=status.HTTP_404_NOT_FOUND)
+
+            if not (author.state == 'ACTIVE'):
+                return JsonResponse({'error': 'This user cannot login to the system.'},
+                                    status=status.HTTP_401_UNAUTHORIZED)
+
+            login(request, auth_user)
+            response = {
+                'message': 'User Logged in Successfully.',
+                'user': {
+                    'username': author.username,
+                    'displayName': author.displayName,
+                    'is_node_admin': auth_user.is_staff,
+                    'author_id': author.id
+                }
+            }
+            return JsonResponse(response, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'error': 'Incorrect Username or Password'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+
