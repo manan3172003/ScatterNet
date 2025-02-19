25656c57db86aa743d215a7167d82c731c3f2ba7
Manan Jain <mananj146@gmail.com>
2025-02-19 16:58:50 -0700

    commiting updates making GET for author posts

--- backend/apps/posts/views.py
+++ backend/apps/posts/views.py
@@ -5,6 +5,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from ..utils.paginators import PostsPaginator
 
 
 # Create your views here.
@@ -13,18 +16,36 @@ def get_post(request, url_id):
     decoded_url = unquote(url_id)
     try:
         post = Post.objects.get(id_url=decoded_url)
-        if post.visibility != 'PUBLIC':
+        if post.visibility == 'FRIENDS':
+            # validate the user is friend of author or is author
+            print("Are Friends")
+        elif post.visibility != 'PUBLIC':
             raise Http404
     except Post.DoesNotExist:
         raise Http404
+
     serializer = PostSerializer(post)
     return Response(serializer.data)
 
-@api_view(["GET"])
-def get_posts(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+# @api_view(["GET", "POST"])
+# def get_posts(request):
+#     posts = Post.objects.all()
+#     serializer = PostSerializer(posts, many=True)
+#     return Response(serializer.data)
+
+class PostListCreateView(APIView, ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = PostsPaginator
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            queryset = Post.objects.filter(author_id=kwargs.get("auth_id"),visibility='PUBLIC').values()
+        elif request.user.author_profile
+
+
+
+
 
 @api_view(["POST"])
 def create_post(request, auth_id):
