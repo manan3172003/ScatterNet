385d0fd351830460638587d089d9fe23d97e0b19
meghan <144631876+meghanwickstrand@users.noreply.github.com>
2025-02-20 20:51:44 -0700

    added icons and sample images

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,18 +1,22 @@
+import profilePic from "../assets/sample-images/party.jpg";
+import profileCover from "../assets/sample-images/stadium.jpg";
+import backBtn from "../assets/icons/back.png";
+import editBtn from "../assets/icons/edit.png";
 export default function ProfileHeader() {
   return (
     <header>
       <div class="cover-wrapper">
-        <img id="cover-image" src="assets/stadium.jpg" />
+        <img id="cover-image" src={profileCover} />
         <div class="cover-buttons-wrapper">
           <div id="back-btn">
-            <img src="assets/back.png" />
+            <img src={backBtn} />
           </div>
           <div id="edit-btn">
-            <img src="assets/edit.png" />
+            <img src={editBtn} />
           </div>
         </div>
         <div class="info-wrapper">
-          <img id="profile-image" src="assets/party.jpg" />
+          <img id="profile-image" src={profilePic} />
           <div class="info-names-wrapper">
             <p id="displayname">Meghan</p>
             <div class="info-names-subwrapper">
