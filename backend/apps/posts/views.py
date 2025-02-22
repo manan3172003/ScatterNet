86b5cf829aba0e67fe5d28233cbfa4c34acd7d03
Manan Jain <mananj146@gmail.com>
2025-02-22 01:39:09 -0700

    friends and follows conditions applied

--- backend/apps/posts/views.py
+++ backend/apps/posts/views.py
@@ -1,6 +1,5 @@
 from urllib.parse import unquote
 
-from django.http.response import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,6 +8,7 @@ from .serializers import PostSerializer
 from rest_framework.generics import ListAPIView
 
 from ..authors.models import Author
+from ..utils.helper import are_friends, follows
 from ..utils.paginators import PostsPaginator
 
 
@@ -18,38 +18,43 @@ def get_post(request, url_id):
     decoded_url = unquote(url_id)
     try:
         post = Post.objects.get(id_url=decoded_url)
-        if post.visibility == 'FRIENDS':
-            # validate the user is friend of author or is author
-            print("Are Friends")
-        elif post.visibility != 'PUBLIC':
-            raise Http404
     except Post.DoesNotExist:
-        raise Http404
+        return Response({'error': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
 
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    if post.visibility == 'FRIENDS':
+        if request.user.is_authenticated:
+            friends = are_friends(request.user.author_profile.id, post.author.id)
+            if friends:
+                serializer = PostSerializer(post)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+    elif post.visibility != 'PUBLIC':
+        return Response({'error': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
+    elif post.visibility == 'PUBLIC':
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    return Response({'error': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
 
 def get_author_post(request, auth_id, post_id):
     try:
         author = Author.objects.get(id=auth_id)
         if author.state != "ACTIVE":
-            return Response({"error": "Author not found"}, status=404)
+            return Response({"error": "Author not found"}, status=status.HTTP_404_NOT_FOUND)
     except Author.DoesNotExist:
-        return Response({"error": "Author not found"}, status=404)
+        return Response({"error": "Author not found"}, status=status.HTTP_404_NOT_FOUND)
 
     try:
         post = Post.objects.get(id=post_id)
     except Post.DoesNotExist:
-        return Response({"error": "Post not found"}, status=404)
+        return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
 
     if post.visibility == 'PUBLIC' or post.visibility == 'UNLISTED':
         serializer = PostSerializer(post)
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif post.visibility == 'FRIENDS':
-        # Need to check for friends validation
-        print("Are Friends")
-        serializer = PostSerializer(post)
-        return Response(serializer.data, status=200)
+        if are_friends(request.user.author_profile.id, post.author.id):
+            serializer = PostSerializer(post)
+            return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         if request.user.is_authenticated and request.user.is_staff:
             serializer = PostSerializer(post)
@@ -80,7 +85,7 @@ def delete_author_post(request, auth_id, post_id):
     if not request.user.is_authenticated:
         return Response({'error': 'Need to be logged in to delete a post'}, status=status.HTTP_401_UNAUTHORIZED)
 
-    if request.user.author_profile.id != auth_id:
+    if request.user.author_profile.id != auth_id and not request.user.is_staff:
         return Response({'error': 'Incorrect author'}, status=status.HTTP_403_FORBIDDEN)
 
     try:
@@ -102,14 +107,6 @@ def author_post(request, auth_id, post_id):
     elif request.method == 'DELETE':
         return delete_author_post(request, auth_id, post_id)
 
-# @api_view(["GET"])
-# def get_posts(request):
-#     try:
-#         posts = Post.objects.all()
-#     except Post.DoesNotExist:
-#         return Response({})
-#     serializer = PostSerializer(posts, many=True)
-#     return Response(serializer.data)
 
 class PostListCreateView(ListAPIView):
     serializer_class = PostSerializer
@@ -122,23 +119,29 @@ class PostListCreateView(ListAPIView):
 
         if not self.request.user.is_authenticated:
             return  queryset.filter(visibility='PUBLIC')
+
         elif self.request.user.author_profile.id == auth_id and not self.request.user.is_staff:
             return queryset
         elif self.request.user.is_staff:
             return Post.objects.filter(author_id=auth_id)
         elif self.request.user.author_profile.id != auth_id:
-            # need to validate based on friends and followers
-            return queryset
+            if are_friends(self.request.user.author_profile.id, auth_id):
+                return queryset.filter(visibility='FRIENDS')
+            elif follows(self.request.user.author_profile.id, auth_id):
+                return queryset.filter(visibility__in=['PUBLIC', 'UNLISTED'])
+            else:
+                return queryset.filter(visibility='PUBLIC')
+
 
     def post(self, request, *args, **kwargs):
         auth_id = self.kwargs.get('auth_id')
 
-        if not self.request.user.is_authenticated or auth_id != self.request.user.author_profile.id:
-            return Response(status=401)
-        elif self.request.user.author_profile.id == auth_id:
+        if not self.request.user.is_authenticated or auth_id != self.request.user.author_profile.id or not self.request.user.is_staff:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        elif self.request.user.author_profile.id == auth_id or self.request.user.author_profile.is_staff:
             serializer = PostSerializer(data=request.data, context={'auth_id': auth_id})
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data, status=201)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
             else:
-                return Response(serializer.errors, status=400)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
