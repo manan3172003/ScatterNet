a2fd5cbf40ea3f2c6f8280c8ae5a111062086bb0
Manan Jain <mananj146@gmail.com>
2025-02-21 22:37:37 -0700

    fixed requirements and some WIP

---
+++ backend/apps/utils/helper.py
@@ -0,0 +1,5 @@
+# from ..follow.models import Follow
+#
+# def are_friends(author1_id, author2_id):
+#     try:
+#         follows_1_2 = Follow.objects.get(actor_id=author1_id, object_id=author2_id)
