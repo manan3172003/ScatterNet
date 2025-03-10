41e290659b937b31b44782901eaa92163c3c21bd
Karl Vinuya <karleric@ualberta.ca>
2025-03-09 22:58:34 -0600

    Add follow button on post component

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -14,6 +14,7 @@ export default function Post({post, onPostClick,onCommentClick, hideCommentsButt
     const [likeCount,setLikeCount] = useState(post.likes.count)
     const [commentCount,setCommentCount] = useState(post.comments.count)
     const [hasLiked, setLikes] = useState(false) // Default to false
+    const [authorsRelationship, setAuthorsRelationship] = useState("Following");
 
     // This state is going keep track of whether or not the post has been expanded since by default we truncate excess text 
     const [expanded, setExpanded] = useState(false) 
@@ -26,12 +27,14 @@ export default function Post({post, onPostClick,onCommentClick, hideCommentsButt
     })
 
     useEffect(() => {
-        async function fetchLikeStatus() {
+        async function fetchLikeAndFollowStatus() {
             const liked = await getLikeStatus(user);
+            const relationship = await isAuthorFollowed(user);
             setLikes(liked);
+            setAuthorsRelationship(relationship);
         }
         if (user) {
-            fetchLikeStatus();
+            fetchLikeAndFollowStatus();
         }
     }, []);
     async function handleLike() {
@@ -63,7 +66,56 @@ export default function Post({post, onPostClick,onCommentClick, hideCommentsButt
           }
     }
 
+    // WE DONT HAVE A WAY TO CANCEL FOLLOW REQS, not in spec either
+    async function handleFollow() {
 
+        // TODO: REMOVE THIS BEABADOBEE NEXT SPLIT, ONLY WORKS LOCALLY, LETS THINK OF SMT
+        const parts = post.author.id.split("/");
+        const postAuthorId = parts[parts.length - 1];
+
+        const userAuthor =  await getAuthorObject(user);
+
+        switch(authorsRelationship) {
+            case "Following": {
+                const response = await fetch(`http://localhost:8000/api/authors/${postAuthorId}/followers/${userAuthor.id}`, {
+                method: "DELETE",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": csrfToken
+                    },
+                });
+
+                if (response.ok) {
+                    setAuthorsRelationship("Not Following");
+                    console.log("Unfollowed successfully");
+                }
+                else {
+                    console.log("Error unfollowing");
+                }
+                break;
+            }
+            case "Not Following": {
+                const response = await fetch(`http://localhost:8000/api/authors/${postAuthorId}/followers/${userAuthor.id}`, {
+                method: "PUT",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": csrfToken
+                    },
+                });
+
+                if (response.ok) {
+                    setAuthorsRelationship("Requested");
+                    console.log("Follow request sent successfully");
+                }
+                else {
+                    console.log("Error sending follow request");
+                }
+                break;
+            }
+        }
+    }
     
     function handleShare(){
         if (navigator.clipboard) {
@@ -71,6 +123,45 @@ export default function Post({post, onPostClick,onCommentClick, hideCommentsButt
                 .then(() => alert("Post URL copied to clipboard!"))
                 .catch(err => console.error("Failed to copy URL", err));
           }
+    }
+
+    async function isAuthorFollowed(user){
+        try {
+            // Check if post author is in following list of user
+            const followerResponse = await fetch(`http://localhost:8000/api/authors/${user.author_id}/following?isPending=false`);
+            const followerData = await followerResponse.json();
+
+            if (followerData.following.some(author => author.id === post.author.id)) {
+                return "Following";
+            }
+
+            // Check if there is a pending request to post author
+            const followReqResponse = await fetch(`http://localhost:8000/api/authors/${user.author_id}/following?isPending=true`);
+            const followReqData = await followReqResponse.json();
+
+            if (followReqData.following.some(author => author.id === post.author.id)) {
+                return "Requested";
+            }
+
+            return "Not Following";
+        }
+        catch (error) {
+            console.error("Error checking follow status:", error);
+            return false;
+        }
+    }
+
+    function getButtonLabel(relationship) {
+        switch(relationship) {
+            case "Following":
+                return "Unfollow";
+
+            case "Not Following":
+                return "Follow";
+
+            case "Requested":
+                return "Requested";
+        }
     }
     
     async function getLikeStatus(user) {
@@ -123,11 +214,18 @@ export default function Post({post, onPostClick,onCommentClick, hideCommentsButt
                   <span className="post-date">{formattedDate}</span>
                 </div>
               </div>
+                <button
+                    className="post-follow-button"
+                    onClick={handleFollow}
+                    disabled={authorsRelationship === "Requested"}
+                  >
+                        {getButtonLabel(authorsRelationship)}
+                  </button>
             </div>
-            
             {post.description && (
               <p className="post-description">{post.description}</p>
             )}
+
           </div>
     
           <div className="post-content" onClick={onPostClick}>
