c279611d77b4cb4a1a719aefe148288c05828e39
Karl Vinuya <karleric@ualberta.ca>
2025-03-22 23:21:04 -0600

    update comment count when modal closes

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -8,11 +8,12 @@ import {useNavigate} from 'react-router-dom';
 import {Calendar, Globe, Heart, Trash, Link, Lock, MessageCircle, Share2} from "lucide-react"
 import {getAuthorRelationship, handleFollowRequest} from "../utils/followApi.js";
 import {apiCall, getAuthorObject} from "../utils/utils.js";
+import {fetchAllComments} from "../utils/commentApi.js";
 
-export default function Post({ post, onPostClick, onCommentClick, hideCommentsButton = false, hideFollowButton = false, onRefresh }) {
+export default function Post({ post, onPostClick, onCommentClick, hideCommentsButton = false, hideFollowButton = false, onRefresh, isCommentModalOpen }) {
   const { user } = useContext(AuthContext)
   const [likeCount, setLikeCount] = useState(post.likes.count)
-  const [commentCount, setCommentCount] = useState(post.comments.count)
+  const [commentCount, setCommentCount] = useState(0)
   const [hasLiked, setLikes] = useState(false) // Default to false
   const [authorsRelationship, setAuthorsRelationship] = useState("Follow");
 
@@ -36,8 +37,10 @@ export default function Post({ post, onPostClick, onCommentClick, hideCommentsBu
     }
     if (user) {
       fetchLikeAndFollowStatus();
+
+      if (!isCommentModalOpen) fetchCommentCount();
     }
-  }, []);
+  }, [isCommentModalOpen]);
   async function handleLike() {
     if (user === null) {
       // Not logged in so do nothing
@@ -70,7 +73,13 @@ export default function Post({ post, onPostClick, onCommentClick, hideCommentsBu
 
     if (onRefresh) {
       onRefresh();
+    }
   }
+
+  async function fetchCommentCount() {
+    const allComments = await fetchAllComments(post);
+    const fetchedCommentsCount = allComments.length;
+    setCommentCount(fetchedCommentsCount)
   }
 
   function handleShare() {
