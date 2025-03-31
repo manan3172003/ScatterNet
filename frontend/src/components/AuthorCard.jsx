284fb5739a614734d20448120a3c8cf07b885c2c
Manan Jain <mananj146@gmail.com>
2025-03-31 19:06:06 +0000

    discover page somewhat working

--- frontend/src/components/AuthorCard.jsx
+++ frontend/src/components/AuthorCard.jsx
@@ -1,6 +1,18 @@
 import "../assets/styles/author-card.css";
+import { useContext, useState } from "react";
+import {AuthContext} from "../context/AuthContext";
+import {getAuthorObject} from "../utils/utils.js";
+import {sendRemoteFollowRequest} from "../utils/remoteAuthorsApi.js";
 
 export default function AuthorCard({ author }) {
+    const [isFollowing, setIsFollowing] = useState(false);
+    const { user } = useContext(AuthContext);
+
+    async function handleFollow() {
+        const user_author = await getAuthorObject(user);
+        const follow_success = await sendRemoteFollowRequest(user_author, author);
+        setIsFollowing(follow_success);
+    }
 
     return (
         <div className="author-card">
@@ -11,7 +23,12 @@ export default function AuthorCard({ author }) {
             />
             <h3 className="author-name">{author.displayName}</h3>
             <p className="author-host">{author.host}</p>
-            <button className="follow-button">Follow</button>
+            <button
+                className="follow-button"
+                onClick={handleFollow}
+                disabled={isFollowing}
+            >{isFollowing? "Following" : "Follow"}
+            </button>
         </div>
     )
 }
--- frontend/src/utils/remoteAuthorsApi.js
+++ frontend/src/utils/remoteAuthorsApi.js
@@ -2,7 +2,7 @@ import {apiCall, fetchUserData, getAuthorObject} from "./utils.js";
 
 export async function fetchDiscoverAuthors() {
     try {
-        const response = await apiCall(`authors`); // TODO: change this to authors/discover
+        const response = await apiCall(`authors/discover`); // TODO: change this to authors/discover
 
         if (response.ok) {
             return await response.json();
@@ -10,42 +10,6 @@ export async function fetchDiscoverAuthors() {
     }
     catch (error) {
         console.error("Error fetching remote authors:", error);
-    }
-}
-
-// TODO: change this to remote authors
-export async function getAuthorRelationship(otherAuthor){
-    let {user: user} = await fetchUserData();
-    user = await getAuthorObject(user);
-
-    // First condition is when we pass the whole author object
-    // second is when we have the otherAuthor's author id (e.g., ProfileHeader)
-    if (user.id === otherAuthor.id) {
-        return "Same Author";
-    }
-
-    try {
-        // Check if other author is in following list of user
-        const followerResponse = await apiCall(`authors/${user.serial}/following?isPending=false`);
-        const followerData = await followerResponse.json();
-
-        if (followerData.following.some(author => author.id === otherAuthor.id)) {
-            return "Following";
-        }
-
-        // Check if there is a pending request to other author
-        const followReqResponse = await apiCall(`authors/${user.serial}/following?isPending=true`);
-        const followReqData = await followReqResponse.json();
-
-        if (followReqData.following.some(author => author.id === otherAuthor.id)) {
-            return "Requested";
-        }
-
-        return "Not Following";
-    }
-    catch (error) {
-        console.error("Error checking follow status:", error);
-        return false;
     }
 }
 
@@ -60,7 +24,9 @@ export async function sendRemoteFollowRequest(currentAuthor, otherAuthor) {
         const followResponse = await apiCall("authors/discover", "POST", requestBody);
 
         if (followResponse.ok) {
-            return "Following";
+            return true;
+        } else {
+            return false;
         }
     }
 
